1f4aa2d2eac033085d8057d78b11697f3f6cd81a
Ezequiel Oggioni <oggioni@providencia.com.ar>
2023-08-14 21:07:28 -0300

    agregar datos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UsuariosComponent } from './usuarios/usuarios.component';
+
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { provideAuth,getAuth } from '@angular/fire/auth';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsuariosComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    provideFirebaseApp(() => initializeApp({
+      apiKey: "AIzaSyAgbm-R7Rz8lFEtYQEGHtK9XmNwMkG3gyo",
+      authDomain: "beltran-1ae8a.firebaseapp.com",
+      projectId: "beltran-1ae8a",
+      storageBucket: "beltran-1ae8a.appspot.com",
+      messagingSenderId: "926158595015",
+      appId: "1:926158595015:web:0a6adf8ffb761c9d8d6fe2"
+    })),
+    provideFirestore(() => getFirestore()),
+    provideAuth(() => getAuth()),
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  firebase: {
+    projectId: 'beltran-1ae8a',
+    appId: '1:926158595015:web:0a6adf8ffb761c9d8d6fe2',
+    storageBucket: 'beltran-1ae8a.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyAgbm-R7Rz8lFEtYQEGHtK9XmNwMkG3gyo',
+    authDomain: 'beltran-1ae8a.firebaseapp.com',
+    messagingSenderId: '926158595015',
+  },
+};
